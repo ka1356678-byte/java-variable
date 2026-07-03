e21bdd0365ca7a3087dcbab7edc82249aadb7c39
ka1356678-byte <ka1356678@gmail.com>
2026-07-04 01:39:50 +0500

    create commit

---
+++ variables.js
@@ -0,0 +1,20 @@
+let batchName = "Batch 23";
+const batchNumber = 23;
+let isActive = true;
+let color = ["red", "blue", "green", "yellow"]
+let info = {
+    name: "Batch 23",
+    number: 23,
+    isActive: true,
+    colors: ["red", "blue", "green", "yellow"]
+}
+let aboutMe;
+let aboutClass = null;
+
+batchName = "batch 23 TTs ho"
+// batchNumber = 26; not changeable because it is a constant
+console.log(aboutMe);
+console.log(aboutClass);
+console.log(info);
+console.log(batchName);
+console.log(batchNumber);
